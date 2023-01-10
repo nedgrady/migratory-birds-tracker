85d79626b05cdfd65717760c2288be2510b85924
Ned Grady <gradyned@gmail.com>
2023-01-10 00:35:12 +0000

    Remov e junk

--- src/App.tsx
+++ src/App.tsx
@@ -39,23 +39,24 @@ function App() {
 	})
 
 	if (isLoaded) {
-		return <div style={{display: "flex", gap:"10px"}}><GoogleMap
-			center={{ lat: 53.55153, lng: -2.009018 }}
-			zoom={6}
-			onLoad={(map) => {
-				var heatmap = new google.maps.visualization.HeatmapLayer({
-					data: sightings.map(sighting => new google.maps.LatLng(sighting.location.latitude, sighting.location.longitude)),
-					radius: 8,
-					opacity: 0.9
-				});
+		return <div style={{ display: "flex", gap: "10px", flexDirection: "row" }}>
+			<GoogleMap
+				center={{ lat: 53.55153, lng: -2.009018 }}
+				zoom={6}
+				onLoad={(map) => {
+					var heatmap = new google.maps.visualization.HeatmapLayer({
+						data: sightings.map(sighting => new google.maps.LatLng(sighting.location.latitude, sighting.location.longitude)),
+						radius: 8,
+						opacity: 0.9
+					});
 
-				heatmap.setMap(map);
-			}}
-			mapContainerStyle={{ height: "98vh", width: "50%" }}
-		/>
-		<div>
-			<SightingList sightings={sightings} />
-		</div>
+					heatmap.setMap(map);
+				}}
+				mapContainerStyle={{ height: "98vh", width: "50%" }}
+			/>
+			<div>
+				<SightingList sightings={sightings} />
+			</div>
 		</div>
 	}
 
