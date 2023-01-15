a84dce7d7269b4ca76d3f1fffe58b53e5cb66f41
Ned Grady <gradyned@gmail.com>
2023-01-15 13:58:35 +0000

    Try this

--- src/SightingsList.tsx
+++ src/SightingsList.tsx
@@ -8,12 +8,7 @@ import {
 	useReactTable,
 } from "@tanstack/react-table"
 import { format } from "date-fns"
-import Table from "@mui/material/Table"
-import TableBody from "@mui/material/TableBody"
-import TableCell from "@mui/material/TableCell"
-import TableContainer from "@mui/material/TableContainer"
-import TableHead from "@mui/material/TableHead"
-import TableRow from "@mui/material/TableRow"
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 
 const columnHelper = createColumnHelper<Sighting>()
 
@@ -35,29 +30,29 @@ export default function SightingList({ sightings }: { sightings: Sighting[] }) {
 	})
 
 	return (
-		// <TableContainer>
-		<Table>
-			<TableHead>
-				<TableRow>
-					{table.getFlatHeaders().map(header => (
-						<TableCell key={header.id} style={{ textTransform: "capitalize" }}>
-							{flexRender(header.column.columnDef.header, header.getContext())}
-						</TableCell>
-					))}
-				</TableRow>
-			</TableHead>
-			<TableBody>
-				{table.getRowModel().rows.map(row => (
-					<TableRow key={row.id}>
-						{row.getVisibleCells().map(cell => (
-							<TableCell key={cell.id}>
-								{flexRender(cell.column.columnDef.cell, cell.getContext())}
+		<TableContainer>
+			<Table>
+				<TableHead>
+					<TableRow>
+						{table.getFlatHeaders().map(header => (
+							<TableCell key={header.id} style={{ textTransform: "capitalize" }}>
+								{flexRender(header.column.columnDef.header, header.getContext())}
 							</TableCell>
 						))}
 					</TableRow>
-				))}
-			</TableBody>
-		</Table>
-		// </TableContainer>
+				</TableHead>
+				<TableBody>
+					{table.getRowModel().rows.map(row => (
+						<TableRow key={row.id}>
+							{row.getVisibleCells().map(cell => (
+								<TableCell key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</TableCell>
+							))}
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	)
 }
