0014c353ec4f02abe83df7f4ad7848db49eecd57
Ned Grady <gradyned@gmail.com>
2023-01-21 15:49:12 +0000

    Somewhat fix sightings list height on mobile.

--- src/SightingsList.tsx
+++ src/SightingsList.tsx
@@ -1,13 +1,10 @@
 import { Sighting, sightingsAtom } from "./birdData"
-import { createColumnHelper } from "@tanstack/react-table"
 import { format } from "date-fns"
-import { Autocomplete, Button, FormControl, FormGroup, Input, Stack, TextField } from "@mui/material"
-import { DataGrid, GridColDef } from "@mui/x-data-grid"
+import { Stack, useMediaQuery, useTheme } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2"
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker"
-import { usePlacesWidget } from "react-google-autocomplete"
-import { useState } from "react"
+import { DataGrid, GridColDef } from "@mui/x-data-grid"
 import useRecoilArray from "./useRecoilArray"
+import { AddSighting } from "./AddSighting"
 
 const columns: GridColDef<Sighting>[] = [
 	{ field: "source", headerName: "Source" },
@@ -26,89 +23,22 @@ const columns: GridColDef<Sighting>[] = [
 
 export default function SightingList() {
 	const { array: sightings } = useRecoilArray(sightingsAtom)
-	return (
-		<Stack spacing={2} height="100%">
-			<AddSighting />
-			<DataGrid rows={sightings} columns={columns} style={{ flex: 1 }} />
-		</Stack>
-	)
-}
+	const theme = useTheme()
+	const isMobile = useMediaQuery(theme.breakpoints.up("xs"))
 
-function AddSighting() {
-	const [location, setLocation] = useState<google.maps.places.PlaceResult>()
-
-	const { ref } = usePlacesWidget({
-		apiKey: "AIzaSyAryrchXqhY1TtykuGeKPA0BjjOZMcyyAM",
-		onPlaceSelected: placeResult => {
-			setLocation(placeResult)
-			console.log(placeResult)
-		},
-	})
-
-	const { add } = useRecoilArray(sightingsAtom)
-
-	const [date, setDate] = useState<Date | null>(new Date())
-	const [source, setSource] = useState<string>("No Source")
-
-	const handleChange = (newValue: Date | null) => {
-		setDate(newValue)
-	}
-
-	function handleSightingAdded() {
-		const newSighting: Sighting = {
-			bird: "Waxwing",
-			id: new Date().getTime(),
-			location: {
-				description: location?.formatted_address ?? "Nowhere",
-				latitude: location?.geometry?.location?.lat() ?? 0,
-				longitude: location?.geometry?.location?.lng() ?? 0,
-			},
-
-			source: source,
-			timestamp: date ?? new Date(),
-		}
-
-		add(newSighting)
+	const dataGridStyles: React.CSSProperties = {
+		height: isMobile ? "600px" : "100%",
+		flex: isMobile ? "auto" : 1,
 	}
 
 	return (
-		<form>
-			<FormGroup row={true}>
-				<Grid container margin={0} gap={1}>
-					<FormControl component={Grid}>
-						<Autocomplete
-							options={["WLUK", "WSUK"]}
-							renderInput={params => <TextField {...params} label="Source" />}
-							onChange={(_, value) => setSource(value ?? "Nowhere")}
-						/>
-					</FormControl>
-
-					<FormControl component={Grid}>
-						<DesktopDatePicker
-							label="Date"
-							inputFormat="MM/dd/yyyy"
-							renderInput={params => <TextField {...params} />}
-							value={date}
-							onChange={handleChange}
-						/>
-					</FormControl>
-
-					<FormControl component={Grid}>
-						<TextField inputRef={ref} label="Location" />
-					</FormControl>
-
-					<FormControl component={Grid}>
-						<Button
-							variant="outlined"
-							size="large"
-							style={{ height: "100%" }}
-							onClick={handleSightingAdded}
-						>
-							Add
-						</Button>
-					</FormControl>
-				</Grid>
-			</FormGroup>
-		</form>
+		<Grid spacing={2} container direction={{ xs: "column", md: "row" }}>
+			<Grid md={12}>
+				<AddSighting />
+			</Grid>
+			<Grid md={12} flex={1}>
+				<DataGrid rows={sightings} columns={columns} autoHeight={true} />
+			</Grid>
+		</Grid>
 	)
 }
